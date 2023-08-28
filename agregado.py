30df0e15b24c479d3ab9c628f13ae8cf2a35733e
Antonio Macaringue <110739274+Makas-M@users.noreply.github.com>
2023-08-28 15:51:10 +0200

    distritos

--- agregado.py
+++ agregado.py
@@ -1,7 +1,24 @@
 import pandas as pd
+import osmnx as ox
 
-# coluna desejada
-provincia = 'Nampula'
+# distrito desejado
+distrito = 'Chibuto'
+
+# array dos distritos por provincia
+distritoCD = ["Ancuabe","Balama","Chiúre","Ibo","Macomia","Mecúfi","Meluco","Metuge","Mocímboa da Praia","Montepuez","Mueda","Muidumbe","Namuno","Nangade","Palma","Pemba","Quissanga"]
+distritoG = ["Bilene","Chibuto","Chicualacuala","Chigubo","Chókwè","Chongoene","Guijá","Limpopo","Mabalane","Manjacaze","Mapai","Massangena","Massingir","Xai-Xai"]
+distritoI = ["Funhalouro","Govuro","Homoíne","Inhambane","Inharrime","Inhassoro","Jangamo","Mabote","Massinga","Maxixe","Morrumbene", "Panda","Vilanculos","Zavala"]
+distritoMan = ["Bárue","Chimoio","Gondola","Guro","Macate","Machaze","Macossa","Manica","Mossurize","Sussundenga","Tambara","Vanduzi"]
+
+# condicao para seleccao da provincia de acordo com o distrito
+if (distrito in distritoCD):
+    provincia = 'Cabo Delgado'
+elif (distrito in distritoG):
+    provincia = 'Gaza'
+elif (distrito in distritoI):
+    provincia = 'Inhambane'
+elif (distrito in distritoMan):
+    provincia = 'Manica'
 
 # caminhos
 caminho_arquivo = 'agregado.xlsx'
@@ -10,11 +27,12 @@ nome_da_planilha = 'Plan1'
 coluna_agregado = 'Agregado' 
 coluna_provincia = 'Provincia'
 
-# Carregar o arquivo
+# arquivo
 df = pd.read_excel(caminho_arquivo, sheet_name=nome_da_planilha)
 
 # filtrar
-dados_coluna = df[df[coluna_provincia] == provincia]
+dados_coluna = df[df[coluna_provincia] == provincia].values
 
-# Imprimir os dados
+# Imprimir 
 print(dados_coluna)
+print(distrito)
